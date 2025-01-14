47b045c1228fd0880e1605d996099f35b3530c78
idn-fahmifn <fahmifn.idn@gmail.com>
2025-01-14 14:52:44 +0700

    2.5

--- materi/resources/views/karyawan/form.blade.php
+++ materi/resources/views/karyawan/form.blade.php
@@ -11,10 +11,33 @@
 
     <form method="post" action="#" class="mt-6 space-y-6">
         @csrf
+        <div>
+            <x-input-label for="name" :value="__('Nama Lengkap')" />
+            <x-text-input id="nama" name="nama" type="text" class="mt-1 block w-full" required autofocus />
+            <x-input-error class="mt-2" :messages="$errors->get('nama')" />
+        </div>
 
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="nama" name="nama" type="text" class="mt-1 block w-full" required autofocus />
+            <x-input-label for="umur" :value="__('Umur')" />
+            <x-text-input id="umur" name="umur" type="number" class="mt-1 block w-full" required autofocus />
+            <x-input-error class="mt-2" :messages="$errors->get('umur')" />
+        </div>
+
+        <div>
+            <x-input-label for="jabatan" :value="__('Jabatan')" />
+            <x-text-input id="jabatan" name="jabatan" type="text" class="mt-1 block w-full" required autofocus />
+            <x-input-error class="mt-2" :messages="$errors->get('jabatan')" />
+        </div>
+
+        <div>
+            <x-input-label for="nik" :value="__('NIK')" />
+            <x-text-input id="nik" name="nik" type="text" class="mt-1 block w-full" required autofocus />
+            <x-input-error class="mt-2" :messages="$errors->get('nama')" />
+        </div>
+
+        <div>
+            <x-input-label for="alamat" :value="__('Alamat')" />
+            <x-text-input id="alamat" name="alamat" type="text" class="mt-1 block w-full" required autofocus />
             <x-input-error class="mt-2" :messages="$errors->get('nama')" />
         </div>
 
